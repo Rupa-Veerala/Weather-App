a10203e03f93c6dfa43ce2233bca3623d3e39bd5
Veerala-Rupa <veeralarupa@gmail.com>
2024-06-14 10:09:09 +0530

    Updated

--- WeatherApp.py
+++ WeatherApp.py
@@ -12,6 +12,20 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+def getWeather():
+    city = textfield.get()
+
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.geocode(city)
+    obj = TimezoneFinder()
+    result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
+    
+    home=pytz.timezone(result)
+    local_time=datetime.now(home)
+    current_time=local_time.strftime("%I:%M:%P")
+    clock.config(text=current_time)
+    name.config(text="CURRENT WEATHER")
+
 #search box
 Search_image=PhotoImage(file="search.png")
 myimage=Label(image=Search_image)
@@ -22,7 +36,7 @@ textfield.place(x=50,y=40)
 textfield.focus()
 
 Search_icon = PhotoImage(file="search_icon.png")
-myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040")
+myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getWeather)
 myimage_icon.place(x=400,y=34)
 
 #logo
@@ -34,6 +48,12 @@ logo.place(x=150,y=100)
 Frame_image = PhotoImage(file="box.png")
 frame_myimage = Label(image=Frame_image)
 frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
+
+#time
+name = Label(root,font=("arial",15,"bold"))
+name.place(x=30,y=100)
+clock=Label(root,font=("Helvetica",20))
+clock.place(x=30,y=130)
 
 #Label
 label1=Label(root,text="WIND",font=("Helvetica",15,"bold"),fg="purple",bg="#1ab5ef")
