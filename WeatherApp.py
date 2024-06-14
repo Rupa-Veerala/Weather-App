bbab285bbfabce866265ce0a94ee846e3491e20f
Veerala-Rupa <veeralarupa@gmail.com>
2024-06-14 09:09:46 +0530

    App

---
+++ WeatherApp.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import *
+from geopy.geocoders import Nominatim
+from tkinter import ttk, messagebox
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests
+import pytz
+
+root = Tk()
+root.title("Weather App")
+root.geometry("900x500+300+200")
+root.resizable(False,False)
+
+#search box
+
+
+root.mainloop()
